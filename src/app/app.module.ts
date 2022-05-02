7f5eb02895f5167021ba3ab9f689e9e300ae6773
harsh <harsh@gmail.com>
2022-05-02 12:07:50 +0530

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { InputComponent } from './input/input.component';
+import { OutputComponent } from './output/output.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    InputComponent,
+    OutputComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
